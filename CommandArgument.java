dc2084d4150c23bf12e220d0b10e3f2f2070e5da
Vaibhav252525 <keniv935@gmail.com>
2024-01-20 23:06:35 +0530

    initial commit

---
+++ CommandArgument.java
@@ -0,0 +1,14 @@
+
+public class CommandArgument {
+    public static void main(String[] args) {
+        if(args.length>0){
+            System.out.println("arguments are");
+            for (String x: args)
+            System.out.println(x + " ");
+
+        }
+        else
+        System.out.println("no argument");
+
+    }
+}
