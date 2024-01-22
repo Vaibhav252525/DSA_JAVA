40da50861615c48b50ae7f044164e4e2c956dcff
Vaibhav252525 <keniv935@gmail.com>
2024-01-22 16:16:55 +0530

    initial commit

---
+++ Logi_operatorEg.java
@@ -0,0 +1,12 @@
+public class Logi_operatorEg {
+    
+        public static void main(String[] args) {
+            boolean x=true, y= false;
+            System.out.println(x&&y);
+            System.out.println(!y);
+            System.out.println(x||y);
+            
+        }
+    }
+    
+
