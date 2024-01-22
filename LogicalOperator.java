b64c0c900956e3b596a2d56ccd3c42645fc95fc3
Vaibhav252525 <keniv935@gmail.com>
2024-01-22 16:13:15 +0530

    initial commit

---
+++ LogicalOperator.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class LogicalOperator {
+    public static void main(String[] args) {
+        String id="gfg", pass="GFG";
+        Scanner sc= new Scanner(System.in);
+        String iu = sc.next();
+        String pwd = sc.next();
+        if(id.equals(iu) && pass.equals(pwd))
+        System.out.println("Welcome");
+        else
+        System.out.println("Try again");
+        sc.close();
+    }
+
+}
+
+
+    
