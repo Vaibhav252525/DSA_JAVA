94e37016109ee5ba082241815c85ce1b69f6e6e2
Vaibhav252525 <keniv935@gmail.com>
2024-01-27 22:22:17 +0530

    initial commit

---
+++ Product.java
@@ -0,0 +1,9 @@
+public class Product {
+    public static void main(String[] args) {
+        float first = 1.25f;
+        float second = 3.6f;
+        float product = first*second;
+        System.out.print("the product is =" +product);
+
+    }
+}
