e5aa831786255fbc85a8272e8a416352aa11bee1
Vaibhav252525 <keniv935@gmail.com>
2024-01-30 22:13:48 +0530

    increment Y

--- ArithmeticOperator.java
+++ ArithmeticOperator.java
@@ -6,7 +6,10 @@ public class ArithmeticOperator {
         System.out.println(x+y*y);
         int z= x++;
         System.out.println(z+ " " +x);
-        z= ++x;
-        System.out.println(z+ " " +x);
+        z= ++y;
+        System.out.println(z+ " " +y);
     }
 }
+
+// z= x++ first value of x will get assign to the z i.e 10 then get incremented by 1 which will be 11
+// z= y++ here, first value of y will get incremented which will be 31 and this value will get assign to the z 
