240ffd251faa615d67d7673839dd60b98196310e
Vaibhav252525 <keniv935@gmail.com>
2024-01-31 18:34:07 +0530

    initial commit

---
+++ UppertoLower.java
@@ -0,0 +1,12 @@
+public class UppertoLower {
+    public static void main(String args[]) {
+        char ch1 ='P';
+        if(ch1>='a' && ch1<='z'){
+            System.out.println((char)(ch1-'a'+'A')); // converts into upparcase
+        }
+        else{
+            System.out.println((char)(ch1-'A'+'a')); // converts into lowercase
+        }
+        
+    }
+}
