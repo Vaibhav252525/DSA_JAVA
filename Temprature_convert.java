89876217b8aa69e70a7040f011fd929bd39aa441
Vaibhav252525 <keniv935@gmail.com>
2024-02-01 23:32:37 +0530

    initial commit

---
+++ Temprature_convert.java
@@ -0,0 +1,18 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Temprature_convert
+{
+    public static void main (String[] args)
+    {
+        int cel1 = -7;
+        int cel2 = -9;
+        
+        System.out.printf("47 deg Celsius in Fahrenheit:%.2f\n", (9.0/5*cel1 + 32) );
+        System.out.printf("29 deg Celsius in Fahrenheit:%.2f\n", (9.0/5*cel2 + 32) );
+        
+    }
+}
+
+// converting temparature from celcius to fahrenheit
