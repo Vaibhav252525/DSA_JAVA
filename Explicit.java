1dd900c7ca64d1834cd16cd580f4c2ecf5b531c3
Vaibhav252525 <keniv935@gmail.com>
2024-02-02 15:09:44 +0530

    initial commit

---
+++ Explicit.java
@@ -0,0 +1,8 @@
+public class Explicit {
+    public static void main(String[] args) {
+        double a= 45.4;
+        int b=(int)a;
+        System.out.println(b);
+    }
+
+}
---
+++ Implicit.java
@@ -0,0 +1,8 @@
+
+class Implicit{
+    public static void main(String[] args) {
+        int a=20;
+        double b=a;
+        System.out.println(b);
+    }
+}
