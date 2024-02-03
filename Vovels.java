d421a388afafaa836e41f4b04665240e83ab01ab
Vaibhav252525 <keniv935@gmail.com>
2024-02-03 22:20:03 +0530

    initial commit

---
+++ Vovels.java
@@ -0,0 +1,12 @@
+public class Vovels {
+    public static void main(String[] args) {
+        char S1= 'A';
+        if(S1=='A'|| S1=='E'|| S1=='I'||S1=='U'|| S1=='O'){
+        System.out.println("Entered character is vowel");
+        }   
+        else{
+            System.out.println("Entered character is not vowel");
+        }
+        
+    }
+}
