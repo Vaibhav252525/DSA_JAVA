d9e212972222423c58e413aba0dcffe25e1a06d3
Vaibhav252525 <keniv935@gmail.com>
2024-02-05 11:56:33 +0530

    initial commit

---
+++ AreaOFTriangle.java
@@ -0,0 +1,15 @@
+public class AreaOFTriangle {
+    // public static void main(String[] args) {
+    //     int a=6, b=8, c=10;
+    //     double s= (a+b+c)/2.0;
+    //     double Area= (s*(s-a)*(s-b)*(s-c));
+    //     System.out.println(Area);
+    // }
+
+    public static void main(String[] args) {
+        double b=20;
+        double h= 30,Area;
+        Area =(b*h)*1/2;
+        System.out.println(Area);
+    }
+}
