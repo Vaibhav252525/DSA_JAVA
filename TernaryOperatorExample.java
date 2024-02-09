7252d1cc28a1c60f5d7c29eed06e676c0a2e315e
Vaibhav252525 <keniv935@gmail.com>
2024-02-09 20:20:12 +0530

    initial commit

---
+++ TernaryOperatorExample.java
@@ -0,0 +1,13 @@
+public class TernaryOperatorExample {
+        public static void main(String[] args) {
+            int x = 10;
+            int y = 20;
+    
+            // Using the ternary operator to find the maximum of two numbers
+            int max = (x > y) ? x : y;
+    
+            System.out.println("The maximum of " + x + " and " + y + " is: " + max);
+        }
+    }
+    
+
