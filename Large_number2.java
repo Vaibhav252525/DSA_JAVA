9279832cdae599c9016b5e6872004e5fc785209b
Vaibhav252525 <keniv935@gmail.com>
2024-02-16 20:28:29 +0530

    initial commit

---
+++ Large_number2.java
@@ -0,0 +1,16 @@
+public class Large_number2 {
+    public static void main(String[] args) {
+        int a = 12, b = 341, c = 45;
+        if (a>b && a>c) {
+            System.out.println("The largest Number is" + a);
+        }
+            else if (b>c && b>a){
+                System.out.println("The largest Number is" + b);
+            }
+            else{
+                System.out.println("The largest Number is " + c);
+            }
+
+        }
+    }
+
