4f587a140290b0fbd663c3bb95b17deedd965a05
Vaibhav252525 <keniv935@gmail.com>
2024-02-16 20:45:22 +0530

    initial Commit

---
+++ LeapYear.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class LeapYear {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter Year");
+        int L= sc.nextInt();
+        if (L%4==0 && L%400==0) {
+            System.out.println("its a Leap Year");
+            
+        }
+        else{
+        System.out.println("its not leap year");}
+        sc.close();
+
+    }
+}
