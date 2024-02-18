13165e3f36d08335ac99b44c026416eef31a4e18
Vaibhav252525 <keniv935@gmail.com>
2024-02-19 00:00:43 +0530

    initial commit

---
+++ Loops/Do_while.java
@@ -0,0 +1,12 @@
+package Loops;
+
+public class Do_while {
+    public static void main(String[] args) {
+        int i = 0;
+        do {
+            System.out.println("GFG");
+            i++;
+        } while (i < 10);
+
+    }
+}
