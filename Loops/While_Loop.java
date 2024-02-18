1ef8b9b4b71fd773e8c5647ce6495ec72ff5e134
Vaibhav252525 <keniv935@gmail.com>
2024-02-18 23:46:38 +0530

    initial commit

---
+++ Loops/While_Loop.java
@@ -0,0 +1,13 @@
+package Loops;
+
+public class While_Loop {
+    public static void main(String[] args) {
+        int i=0;
+        while (i<5) {
+            System.out.println("GFG");
+            i=i+2;
+            
+        }
+    }
+    
+}
